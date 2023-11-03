5713fd41937e75764083610c7c5f96c7124530f8
Vladislav Valek <vladislav.valek@email.cz>
2023-11-03 15:52:14 +0100

    app_conf [MAINTENANCe]: disable DMA MVB bus

--- build/vcu118/app_conf.tcl
+++ build/vcu118/app_conf.tcl
@@ -22,6 +22,8 @@ set DMA_RX_FRAME_SIZE_MAX 8191
 set DMA_TX_FRAME_SIZE_MAX 8191
 set DMA_TX_DATA_PTR_W 13
 
+set DMA_MVB_EN false
+
 # Optimizes the MFB bus from user logic so upper bits are cut away and only lower
 # 128b are used for data.
 VhdlPkgBool RISCV_BUS_OPT true
