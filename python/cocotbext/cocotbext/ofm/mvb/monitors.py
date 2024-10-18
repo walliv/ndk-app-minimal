f2eac62dc3543e9b55d9bdd33e450060f50ed0c9
Daniel Kondys <xkondy00@vutbr.cz>
2024-10-18 15:44:41 +0200

    Cocotb - MVB monitors [MAINTENANCE]: make monitor's attributes accessible through getters

--- python/cocotbext/cocotbext/ofm/mvb/monitors.py
+++ python/cocotbext/cocotbext/ofm/mvb/monitors.py
@@ -15,21 +15,35 @@ class MVBProtocolError(Exception):
 class MVBMonitor(BusMonitor):
     """
     Master monitor intended for monitoring the MVB bus.
-
-    Atributes:
-        item_cnt(int): number of MVB transaction proccessed.
-        _items(int): number of MVB items.
-        _word_width(int): width of MVB word in bytes.
-        _item_width(int): width of MVB item in bytes.
     """
     _signals = ["data", "vld", "src_rdy", "dst_rdy"]
 
     def __init__(self, entity, name, clock, array_idx=None) -> None:
         super().__init__(entity, name, clock, array_idx=array_idx)
-        self.item_cnt = 0
-        self._items = len(self.bus.vld)
-        self._word_width = len(self.bus.data) // 8  # width in bytes
-        self._item_width = self._word_width // self._items
+        self.__item_cnt = 0
+        self.__items = len(self.bus.vld)
+        self.__word_width = len(self.bus.data) // 8  # width in bytes
+        self.__item_width = self.__word_width // self.__items
+
+    @property
+    def item_cnt(self) -> int:
+        """The number of MVB transactions proccessed."""
+        return self.__item_cnt
+
+    @property
+    def items(self) -> int:
+        """The number of MVB items."""
+        return self.__items
+
+    @property
+    def word_width(self) -> int:
+        """The width of MVB word in bytes."""
+        return self.__word_width
+
+    @property
+    def item_width(self) -> int:
+        """The width of MVB item in bytes."""
+        return self.__item_width
 
     def _is_valid_word(self, signal_src_rdy, signal_dst_rdy) -> bool:
         """Checks if the received word is valid transaction."""
@@ -39,7 +53,7 @@ class MVBMonitor(BusMonitor):
             return (signal_src_rdy.value == 1) and (signal_dst_rdy.value == 1)
 
     def receive_data(self, data, offset):
-        self._recv(data[offset*self._item_width:(offset+1)*self._item_width])
+        self._recv(data[offset*self.__item_width:(offset+1)*self.__item_width])
 
     async def _monitor_recv(self) -> None:
         """Receive function used with cocotb testbench."""
@@ -59,13 +73,13 @@ class MVBMonitor(BusMonitor):
 
                 vld = self.bus.vld.value
 
-                for offset in range(self._items):
-                    if (vld[self._items-offset-1]):
-                        self.log.debug(f"ITEM {self.item_cnt}")
-                        self.log.debug(f"received item: {data_bytes[offset*self._item_width:(offset+1)*self._item_width]}")
+                for offset in range(self.__items):
+                    if (vld[self.__items-offset-1]):
+                        self.log.debug(f"ITEM {self._item_cnt}")
+                        self.log.debug(f"received item: {data_bytes[offset*self.__item_width:(offset+1)*self.__item_width]}")
                         self.log.debug(f"word: {data_bytes}")
-                        self.log.debug(f"item vld: {vld[self._items-offset-1]}")
+                        self.log.debug(f"item vld: {vld[self.__items-offset-1]}")
                         self.log.debug(f"word vld: {vld}")
                         self.receive_data(data_bytes, offset)
 
-                        self.item_cnt += 1
+                        self._item_cnt += 1
