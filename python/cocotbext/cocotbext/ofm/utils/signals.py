14d80000e830fb7f31bef1968cd41fafe3696b50
xschwao00 <xschwao00@stud.fit.vutbr.cz>
2024-11-07 17:43:27 +0100

    docs(cocotb - signals): change style of comments

--- python/cocotbext/cocotbext/ofm/utils/signals.py
+++ python/cocotbext/cocotbext/ofm/utils/signals.py
@@ -5,7 +5,8 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 async def await_signal_sync(clk_re, signal, value: int = 1) -> None:
-    """Synchronously waits until the value of the passed signal becomes passed value.
+    """
+    Synchronously waits until the value of the passed signal becomes passed value.
 
     Note:
         If the value of the signal is already set to passed value, function returns immediately.
@@ -14,15 +15,14 @@ async def await_signal_sync(clk_re, signal, value: int = 1) -> None:
         signal: signal that is to be awaited, must be signal from the cocotb.bus class.
         value: value that is to be awaited.
         clk_re: RisingEdge object.
-
     """
-
     while signal.value != value:
         await clk_re
 
 
 def get_signal_value_in_bytes(signal, big_endian: bool = False) -> bytes:
-    """Get value of a OUT signals in bytes.
+    """
+    Get value of a OUT signals in bytes.
 
     Args:
         signal: cocotb.bus signal that is to be read.
@@ -30,9 +30,7 @@ def get_signal_value_in_bytes(signal, big_endian: bool = False) -> bytes:
 
     Returns:
         Signal value in bytes.
-
     """
-
     sig_val = signal.value
     sig_val.big_endian = big_endian
 
