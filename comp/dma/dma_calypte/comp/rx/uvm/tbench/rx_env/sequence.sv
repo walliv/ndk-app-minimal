a308225010645b7db1dfda03fa41ac94a4707ddb
Radek Isa <isa@cesnet.cz>
2024-11-29 16:31:02 +0100

    perf(dma-calypte-ver): removes intems from queue after driver is ready

--- comp/dma/dma_calypte/comp/rx/uvm/tbench/rx_env/sequence.sv
+++ comp/dma/dma_calypte/comp/rx/uvm/tbench/rx_env/sequence.sv
@@ -15,9 +15,18 @@ class logic_vector_array_sequence#(ITEM_WIDTH) extends uvm_sequence #(uvm_logic_
     endfunction
 
     task body;
+
+        req = uvm_logic_vector_array::sequence_item#(ITEM_WIDTH)::type_id::create("req", this.m_sequencer);
         forever begin
-            tr_export.get(req);
+            uvm_logic_vector_array::sequence_item#(ITEM_WIDTH) hl_tr;
+
+            // Sequence download data from higher level driver and
+            // send them to lower lever driver. Download data only when
+            // data is available and low lever driver is prepared.
+            wait(tr_export.num() != 0);
             start_item(req);
+            tr_export.get(hl_tr);
+            req.copy(hl_tr);
             finish_item(req);
         end
     endtask
@@ -35,9 +44,18 @@ class logic_vector_sequence#(META_WIDTH) extends uvm_sequence #(uvm_logic_vector
     endfunction
 
     task body;
+
+        req = uvm_logic_vector::sequence_item#(META_WIDTH)::type_id::create("req", this.m_sequencer);
         forever begin
-            tr_export.get(req);
+            uvm_logic_vector::sequence_item#(META_WIDTH) hl_tr;
+
+            // Sequence download data from higher level driver and
+            // send them to lower lever driver. Download data only when
+            // data is available and low lever driver is prepared.
+            wait(tr_export.num() != 0);
             start_item(req);
+            tr_export.get(hl_tr);
+            req.copy(hl_tr);
             finish_item(req);
         end
     endtask
