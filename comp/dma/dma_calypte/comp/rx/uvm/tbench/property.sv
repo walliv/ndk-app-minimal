839df9301ad2855d51c01ec57f569438da8d9747
Radek Isa <isa@cesnet.cz>
2024-12-16 16:44:08 +0100

    feat(RX DMA Calypte-VER): add checking if before sof is eof when sof is on higher position that 0

--- comp/dma/dma_calypte/comp/rx/uvm/tbench/property.sv
+++ comp/dma/dma_calypte/comp/rx/uvm/tbench/property.sv
@@ -59,7 +59,20 @@ module DMA_LL_PROPERTY  #(DEVICE, USER_REGIONS, USER_REGION_SIZE, USER_BLOCK_SIZ
         .vif   (mfb_tx)
     );
 
-    //simplyfied rule
+    generate if (PCIE_UP_REGIONS > 1) begin
+        property sof_after_eof;
+            @(posedge mfb_tx.CLK) disable iff(RESET || START)
+            mfb_tx.SRC_RDY |-> (( ~(mfb_tx.EOF[PCIE_UP_REGIONS-2:0]) & mfb_tx.SOF[PCIE_UP_REGIONS-1:1]) == 0);
+        endproperty
+
+        // Check when SOF is not on first position then previous packet have to end in region right before.
+        assert property (sof_after_eof)
+            else begin
+                `uvm_error(module_name, $sformatf("\n\tIf sof is set on different region that 0 then region befor have to be eof set\n\tSOF %b\n\tEOF %b", mfb_tx.SOF, mfb_tx.EOF));
+            end
+    end endgenerate
+
+    //simplyfied rule. No space in middle of packet
     property sof_eof_src_rdy;
         @(posedge mfb_tx.CLK) disable iff(RESET || START)
         (mfb_tx.SRC_RDY && (mfb_tx.SOF != 0)) |-> mfb_tx.SRC_RDY s_until_with (mfb_tx.EOF != 0);
