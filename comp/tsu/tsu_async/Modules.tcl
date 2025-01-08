3c8169db9ae29916e095a555f2dd576fadb6f220
Jakub Cabal <cabal@cesnet.cz>
2025-01-08 15:41:56 +0100

    feat(tsu): re-implement TSU_ASYNC to new version with timeout support

--- comp/tsu/tsu_async/Modules.tcl
+++ comp/tsu/tsu_async/Modules.tcl
@@ -1,49 +1,16 @@
-# Modules.tcl: Local include Modules tcl script
-# Copyright (C) 2009 CESNET
-# Author: Jan Stourac <xstour03 at stud.fit.vutbr.cz>
-#         Jiri Matousek <matousek@cesnet.cz>
+# Modules.tcl: Components include script
+# Copyright (C) 2025 CESNET z.s.p.o.
+# Author(s): Jakub Cabal <cabal@cesnet.cz>
 #
 # SPDX-License-Identifier: BSD-3-Clause
-#
 
+# Paths
+set ASFIFOX_BASE "$OFM_PATH/comp/base/fifo/asfifox"
 
-# Auxiliary paths
-set BASE_BASE              "$COMP_BASE/base"
+# Packages
 
-# Component paths
-set ASFIFO_BASE            "$OFM_PATH/comp/base/fifo/asfifo"
+# Components
+lappend COMPONENTS [list "ASFIFOX" $ASFIFOX_BASE "FULL"]
 
-# Paths to subcomponents with scoped constraints
-set ASYNC_OPEN_LOOP_SMD_BASE "$OFM_PATH/comp/base/async/open_loop_smd"
-
-# Use source files of the module
-if { $ARCHGRP == "SRC" } {
-   # Components
-   set COMPONENTS [list \
-      [list "ASFIFO" $ASFIFO_BASE "FULL"] \
-   ]
-
-   # Modules
-   set MOD "$MOD $ENTITY_BASE/tsu_async.vhd"
-}
-
-# Use .dcp file of the module
-# (Add also all subcomponents with scoped constraints. This hack is necessary
-#  for correct constraint's application in full design.)
-if { $ARCHGRP == "DCP" } {
-   # Components
-   set COMPONENTS [ list \
-      [list "ASYNC_OPEN_LOOP_SMD" $ASYNC_OPEN_LOOP_SMD_BASE "FULL"] \
-      [list "ASFIFO"              $ASFIFO_BASE              "FULL"] \
-   ]
-
-   # Modules
-   set MOD "$MOD $ENTITY_BASE/synth/tsu_async.dcp"
-
-   # All device tree scripts used by module
-   if { [file exists "$ENTITY_BASE/synth/DevTree_paths.txt"] } {
-      set MOD "$MOD [read [open "$ENTITY_BASE/synth/DevTree_paths.txt" "r"]]"
-   }
-}
-
-lappend SRCS(CONSTR_VIVADO) [list $ENTITY_BASE/tsu_async_ooc.xdc USED_IN {synthesis out_of_context}]
+# Files
+lappend MOD "$ENTITY_BASE/tsu_async.vhd"
