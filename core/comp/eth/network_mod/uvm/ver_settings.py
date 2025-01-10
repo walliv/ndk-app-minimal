fc0adeb6688e74a761aeeefd19e3468e1b6c337e
Jakub Cabal <cabal@cesnet.cz>
2025-01-10 12:44:13 +0100

    chore(net-mod-uvm): limit the number of combinations tested in UVM verification

--- core/comp/eth/network_mod/uvm/ver_settings.py
+++ core/comp/eth/network_mod/uvm/ver_settings.py
@@ -150,43 +150,29 @@ SETTINGS = {
 
     "_combinations_" : {
         # 2x E-Tile 100G
-        "2p_etile_1x100g4_normal"       : ("etile_1x100g4", "ports_2",),
-        "2p_etile_1x100g4_small"        : ("etile_1x100g4", "ports_2", "small_packets",),
-        "2p_etile_1x100g4_large"        : ("etile_1x100g4", "ports_2", "large_packets",),
-        "2p_etile_1x100g4_normal_speed" : ("etile_1x100g4", "ports_2", "small_packets", "etile_test_speed",),
-        "2p_etile_1x100g4_small_speed"  : ("etile_1x100g4", "ports_2", "large_packets", "etile_test_speed",),
+        "2p_etile_1x100g4_normal" : ("etile_1x100g4", "ports_2",),
+        "2p_etile_1x100g4_small"  : ("etile_1x100g4", "ports_2", "small_packets",),
+        "2p_etile_1x100g4_large"  : ("etile_1x100g4", "ports_2", "large_packets",),
+        "2p_etile_1x100g4_speed"  : ("etile_1x100g4", "ports_2", "etile_test_speed",),
 
         # TODO UNSUPPORTED: ONLY ONE CHANNEL PER PORT
         # 2x E-Tile 4x10G
-        #"2p_etile_4x10g1_normal"        : ("etile_4x10g1", "ports_2",),
-        #"2p_etile_4x10g1_small"         : ("etile_4x10g1", "ports_2", "small_packets",),
-        #"2p_etile_4x10g1_large"         : ("etile_4x10g1", "ports_2", "large_packets",),
-        #"2p_etile_4x10g1_normal_speed"  : ("etile_4x10g1", "ports_2", "small_packets", "etile_test_speed",),
-        #"2p_etile_4x10g1_small_speed"   : ("etile_4x10g1", "ports_2", "large_packets", "etile_test_speed",),
+        #"2p_etile_4x10g1_normal" : ("etile_4x10g1", "ports_2",),
+        #"2p_etile_4x10g1_small"  : ("etile_4x10g1", "ports_2", "small_packets",),
+        #"2p_etile_4x10g1_large"  : ("etile_4x10g1", "ports_2", "large_packets",),
+        #"2p_etile_4x10g1_speed"  : ("etile_4x10g1", "ports_2","etile_test_speed",),
 
         # 1x F-Tile 400G
-        "1p_ftile_1x400g8_normal"       : ("ftile_1x400g8", "ports_1",),
-        "1p_ftile_1x400g8_small"        : ("ftile_1x400g8", "ports_1", "small_packets",),
-        "1p_ftile_1x400g8_large"        : ("ftile_1x400g8", "ports_1", "large_packets",),
-        "1p_ftile_1x400g8_normal_speed" : ("ftile_1x400g8", "ports_1", "small_packets", "ftile_test_speed",),
-        "1p_ftile_1x400g8_small_speed"  : ("ftile_1x400g8", "ports_1", "large_packets", "ftile_test_speed",),
+        "1p_ftile_1x400g8_normal" : ("ftile_1x400g8", "ports_1",),
+        "1p_ftile_1x400g8_large"  : ("ftile_1x400g8", "ports_1", "large_packets",),
+        "1p_ftile_1x400g8_speed"  : ("ftile_1x400g8", "ports_1", "ftile_test_speed",),
 
         # 1x F-Tile 2x100G
-        "1p_ftile_1x100g4_normal"       : ("ftile_1x100g4", "ports_1",),
-        "1p_ftile_1x100g4_small"        : ("ftile_1x100g4", "ports_1", "small_packets",),
-        "1p_ftile_1x100g4_large"        : ("ftile_1x100g4", "ports_1", "large_packets",),
-        "1p_ftile_1x100g4_normal_speed" : ("ftile_1x100g4", "ports_1", "small_packets", "ftile_test_speed",),
-        "1p_ftile_1x100g4_small_speed"  : ("ftile_1x100g4", "ports_1", "large_packets", "ftile_test_speed",),
+        "1p_ftile_1x100g4_normal" : ("ftile_1x100g4", "ports_1",),
 
         # 2x CMAC 100G
-        "2p_cmac_1x100g4_normal"       : ("cmac_1x100g4", "ports_2",),
-        "2p_cmac_1x100g4_small"        : ("cmac_1x100g4", "ports_2", "small_packets",),
-        "2p_cmac_1x100g4_large"        : ("cmac_1x100g4", "ports_2", "large_packets",),
-        "2p_cmac_1x100g4_normal_speed" : ("cmac_1x100g4", "ports_2", "small_packets", "cmac_test_speed",),
-        "2p_cmac_1x100g4_small_speed"  : ("cmac_1x100g4", "ports_2", "large_packets", "cmac_test_speed",),
-
-        # 4x CMAC 100G
-        "4p_cmac_1x100g4_normal"       : ("cmac_1x100g4", "ports_4",),
-        "4p_cmac_1x100g4_small"        : ("cmac_1x100g4", "ports_4", "small_packets",),
+        "2p_cmac_1x100g4_normal"  : ("cmac_1x100g4", "ports_2",),
+        "2p_cmac_1x100g4_large"   : ("cmac_1x100g4", "ports_2", "large_packets",),
+        "2p_cmac_1x100g4_speed"   : ("cmac_1x100g4", "ports_2", "cmac_test_speed",),
     },
 }
